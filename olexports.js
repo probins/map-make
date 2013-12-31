c2342f1e9222a9d082e94cd948b8104ee315a8b2
Peter Robins <github@peterrobins.co.uk>
2013-12-31 14:46:32 -0500

    Add olexports

---
+++ olexports.js
@@ -0,0 +1,194 @@
+goog.exportSymbol(
+    'ol.Attribution',
+    ol.Attribution);
+
+goog.exportSymbol(
+    'ol.Collection',
+    ol.Collection);
+goog.exportProperty(
+    ol.Collection.prototype,
+    'getArray',
+    ol.Collection.prototype.getArray);
+goog.exportProperty(
+    ol.Collection.prototype,
+    'getAt',
+    ol.Collection.prototype.getAt);
+
+goog.exportSymbol(
+    'ol.control.MousePosition',
+    ol.control.MousePosition);
+goog.exportSymbol(
+    'ol.control.ScaleLine',
+    ol.control.ScaleLine);
+
+goog.exportSymbol(
+    'ol.coordinate.format',
+    ol.coordinate.format);
+goog.exportSymbol(
+    'ol.coordinate.toStringHDMS',
+    ol.coordinate.toStringHDMS);
+goog.exportSymbol(
+    'ol.coordinate.toStringXY',
+    ol.coordinate.toStringXY);
+
+goog.exportSymbol(
+    'ol.extent',
+    ol.extent);
+goog.exportSymbol(
+    'ol.extent.createEmpty',
+    ol.extent.createEmpty);
+goog.exportSymbol(
+    'ol.extent.extend',
+    ol.extent.extend);
+goog.exportSymbol(
+    'ol.extent.getCenter',
+    ol.extent.getCenter);
+goog.exportSymbol(
+    'ol.extent.getTopLeft',
+    ol.extent.getTopLeft);
+goog.exportSymbol(
+    'ol.extent.transform',
+    ol.extent.transform);
+
+goog.exportSymbol(
+    'ol.Feature',
+    ol.Feature);
+goog.exportProperty(
+    ol.Feature.prototype,
+    'getAttributes',
+    ol.Feature.prototype.getAttributes);
+goog.exportProperty(
+    ol.Feature.prototype,
+    'getGeometry',
+    ol.Feature.prototype.getGeometry);
+
+goog.exportSymbol(
+    'ol.layer.Layer',
+    ol.layer.Layer);
+goog.exportProperty(
+    ol.layer.Layer.prototype,
+    'getSource',
+    ol.layer.Layer.prototype.getSource);
+goog.exportSymbol(
+    'ol.layer.Tile',
+    ol.layer.Tile);
+goog.exportSymbol(
+    'ol.layer.Vector',
+    ol.layer.Vector);
+
+goog.exportSymbol(
+    'ol.Map',
+    ol.Map);
+goog.exportProperty(
+    ol.Map.prototype,
+    'addControl',
+    ol.Map.prototype.addControl);
+goog.exportProperty(
+    ol.Map.prototype,
+    'addLayer',
+    ol.Map.prototype.addLayer);
+goog.exportProperty(
+    ol.Map.prototype,
+    'addOverlay',
+    ol.Map.prototype.addOverlay);
+goog.exportProperty(
+    ol.Map.prototype,
+    'getLayers',
+    ol.Map.prototype.getLayers);
+goog.exportProperty(
+    ol.Map.prototype,
+    'getSize',
+    ol.Map.prototype.getSize);
+
+goog.exportSymbol(
+    'ol.Overlay',
+    ol.Overlay);
+goog.exportSymbol(
+    'ol.OverlayPositioning',
+    ol.OverlayPositioning);
+goog.exportProperty(
+    ol.OverlayPositioning,
+    'BOTTOM_CENTER',
+    ol.OverlayPositioning.BOTTOM_CENTER);
+
+goog.exportSymbol(
+    'ol.parser.GeoJSON',
+    ol.parser.GeoJSON);
+goog.exportSymbol(
+    'ol.parser.GPX',
+    ol.parser.GPX);
+goog.exportSymbol(
+    'ol.parser.KML',
+    ol.parser.KML);
+
+goog.exportSymbol(
+    'ol.proj.getTransform',
+    ol.proj.getTransform);
+goog.exportSymbol(
+    'ol.proj.transform',
+    ol.proj.transform);
+goog.exportSymbol(
+    'ol.proj.Projection',
+    ol.proj.Projection);
+goog.exportProperty(
+    ol.proj.Projection.prototype,
+    'getCode',
+    ol.proj.Projection.prototype.getCode);
+
+goog.exportSymbol(
+    'ol.RendererHint',
+    ol.RendererHint);
+goog.exportProperty(
+    ol.RendererHint,
+    'CANVAS',
+    ol.RendererHint.CANVAS);
+
+goog.exportSymbol(
+    'ol.source.BingMaps',
+    ol.source.BingMaps);
+goog.exportSymbol(
+    'ol.source.OSM',
+    ol.source.OSM);
+goog.exportSymbol(
+    'ol.source.TileWMS',
+    ol.source.TileWMS);
+goog.exportSymbol(
+    'ol.source.Vector',
+    ol.source.Vector);
+goog.exportSymbol(
+    'ol.source.WMTS',
+    ol.source.WMTS);
+goog.exportProperty(
+    ol.source.Source.prototype,
+    'getProjection',
+    ol.source.Source.prototype.getProjection);
+goog.exportProperty(
+    ol.source.vector.prototype,
+    'getFeatures',
+    ol.source.vector.prototype.getFeatures);
+
+goog.exportSymbol(
+    'ol.style.Stroke',
+    ol.style.Stroke);
+goog.exportSymbol(
+    'ol.style.Style',
+    ol.style.Style);
+
+goog.exportSymbol(
+    'ol.tilegrid.TileGrid',
+    ol.tilegrid.TileGrid);
+goog.exportSymbol(
+    'ol.tilegrid.WMTS',
+    ol.tilegrid.WMTS);
+
+goog.exportSymbol(
+    'ol.View2D',
+    ol.View2D);
+goog.exportProperty(
+    ol.View2D.prototype,
+    'calculateExtent',
+    ol.View2D.prototype.calculateExtent);
+goog.exportProperty(
+    ol.View2D.prototype,
+    'fitExtent',
+    ol.View2D.prototype.fitExtent);
