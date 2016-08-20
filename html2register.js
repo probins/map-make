9b051564a79fe159e3c5cacb5481e442ee25b958
Peter Robins <github@peterrobins.co.uk>
2016-08-20 21:29:39 +0000

    Change html2register to use argv filename

--- html2register.js
+++ html2register.js
@@ -1,10 +1,12 @@
 "use strict";
 let fs = require('fs');
-fs.readFile('lib/registry/components/toolbar.html', 'utf8', function (err, data) {
+let component = process.argv[2];
+fs.readFile('lib/registry/components/' + component + '.html', 'utf8', function (err, data) {
   if (err) {
     return console.log(err);
   }
-  fs.writeFile('register/registry/components/toolbar.htm.js', '"use strict";System.register([],function(_export,_context){return{setters:[],execute:function(){_export("default",'+JSON.stringify(data)+');}};});', function(err) {
+  fs.writeFile('register/registry/components/' + component + '.htm.js',
+      '"use strict";System.register([],function(_export,_context){return{setters:[],execute:function(){_export("default",'+JSON.stringify(data)+');}};});', function(err) {
     if(err) {
       console.log(err);
     } else {
