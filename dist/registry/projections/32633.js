67a08f6b5337b046f9d51d9a4f6fbe3a7b212880
Peter Robins <gitlab@peterrobins.co.uk>
2020-12-29 16:00:08 +0000

    Fix 32633 on myproj

--- dist/registry/projections/32633.js
+++ dist/registry/projections/32633.js
@@ -1,1 +1,1 @@
-import o from"https://cdn.jsdelivr.net/gh/probins/myproj@0.4.0/defs/epsg32633.js";import"https://cdn.jsdelivr.net/gh/probins/myproj@0.4.0/dist/esbare/utm.js";import r from"./common.js";r.addProjection("EPSG:32633",o);export default{projCode:"EPSG:32633"};
+import o from"https://cdn.jsdelivr.net/gh/probins/myproj@0.4.2/defs/epsg32633.js";import"https://cdn.jsdelivr.net/gh/probins/myproj@0.4.0/dist/esbare/utm.js";import r from"./common.js";r.addProjection("EPSG:32633",o);export default{projCode:"EPSG:32633"};
